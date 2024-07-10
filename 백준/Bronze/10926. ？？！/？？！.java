6f29508b6481f687f11adcbdc7e727a034b14763
Doyun Jang <137027134+doyun034@users.noreply.github.com>
2024-07-11 01:08:25 +0900

    [Bronze V] Title: ??!, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String A = "??!";
+
+        Scanner sc = new Scanner(System.in);
+        String B = sc.nextLine();
+
+        System.out.println(B + A);
+    }
+}
